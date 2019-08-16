5af40ec1fbd32ee10dd8d97481f60b60ea4083b4
Raphael <raphael@scientie.com>
2019-08-16 15:09:20 -0300

    Adicionado Vídeo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,22 @@
-$(function(){
-    $(window).scroll(function(){
+$(function () {
+    $(window).scroll(function () {
         var height = $(window).scrollTop();
-        if(height > 30) {
-            $( ".navbar" ).addClass( "scroll" );
-        }else{
-            $( ".navbar" ).removeClass( "scroll" );
+        if (height > 30) {
+            $(".navbar").addClass("scroll");
+        } else {
+            $(".navbar").removeClass("scroll");
         }
     });
-    $('.navbar-toggler').on('click', function() {
+    $('.navbar-toggler').on('click', function () {
         $('.navbar-collapse').toggleClass('show');
-      });
-  });
+    });
+    $('.videoBackground').click(function () {
+        var src = 'https://www.youtube.com/embed/RkQzRUBBSVQ';
+        $('#videoBackground').modal('show');
+        $('#videoBackground iframe').attr('src', src);
+    });
+
+    $('#videoModal button').click(function () {
+        $('#videoModal iframe').removeAttr('src');
+    });
+});
