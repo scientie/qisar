91489f5fc8e49909a1e664aac4908e4a1f0b13de
Raphael <raphael@scientie.com>
2019-08-16 15:25:12 -0300

    Youtube

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,13 +10,18 @@ $(function () {
     $('.navbar-toggler').on('click', function () {
         $('.navbar-collapse').toggleClass('show');
     });
-    $('.videoBackground').click(function () {
-        var src = 'https://www.youtube.com/embed/RkQzRUBBSVQ';
-        $('#videoBackground').modal('show');
-        $('#videoBackground iframe').attr('src', src);
+    var $videoSrc;
+    $('.video-btn').click(function () {
+        $videoSrc = "https://www.youtube.com/embed/RkQzRUBBSVQ";
     });
+    $('.videoBackground').on('shown', function (e) {
 
-    $('#videoModal button').click(function () {
-        $('#videoModal iframe').removeAttr('src');
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', "https://www.youtube.com/embed/RkQzRUBBSVQ?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    });
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
     });
 });
